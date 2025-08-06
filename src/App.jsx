581d179e9cf514fc540aba687c5de59496d6cfcd
alinakassym <alinakassym@gmail.com>
2025-08-06 17:10:56 +0500

    feat: add User component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { User } from "./components";
 import styles from "./App.module.css";
 
 function App() {
-  return <div className={styles.App}></div>;
+  return (
+    <div className={styles.App}>
+      <User />
+    </div>
+  );
 }
 
 export default App;
