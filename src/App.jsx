930c1149dcd1306678418fddec0e3f857c91cdc7
alinakassym <alinakassym@gmail.com>
2025-08-06 18:13:10 +0500

    feat: add ControlPanel component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { User } from "./components";
+import { User, ControlPanel } from "./components";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.App}>
       <User />
+      <ControlPanel />
     </div>
   );
 }
