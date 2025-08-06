4da46dfa3f26fd168dbce06bd9980ad9536e29ba
alinakassym <alinakassym@gmail.com>
2025-08-06 21:17:04 +0500

    feat: add redux-thunk middleware to store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 import { userReducer, productsReducer } from "./reducers";
 const reducer = combineReducers({
   user: userReducer,
   products: productsReducer,
 });
-export const store = createStore(reducer);
+export const store = createStore(reducer, applyMiddleware(thunk));
